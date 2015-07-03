82a6121af84c2f96a299017ff07eca55caeeb5cb
YanLobat <yanlavryushev@gmail.com>
2015-07-03 15:22:28 +0400

    testi

--- builds/development/app/app.js
+++ builds/development/app/app.js
@@ -22,20 +22,3 @@ $.material.init();
 				$logProvider.debugEnabled(true); //включение дебага в нашем приложениии
 		}
 })();
-;(function(){
-	'use strict';
-	angular
-		.module('ngFit.fitfire.service',['ngRoute','firebase'])
-		.service('fitfire', fitfire);
-		fitfire.$inject = ['FIREBASE_URL','$firebaseObject','$firebaseArray'];
-		function fitfire(FIREBASE_URL,$firebaseObject,$firebaseArray){
-			var self = this;
-
-			var db = new Firebase(FIREBASE_URL);
-			var db_obj = $firebaseObject(db);
-
-			db_obj.$loaded(function(){
-				self.db_obj = db_obj;					
-			});
-		};
-})();
