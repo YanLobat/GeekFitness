e172f86170c49b2a8c5b8d0ef8f983d16d3523bc
YanLobat <yanlavryushev@gmail.com>
2015-11-24 06:50:09 +0400

    auth fix

--- builds/development/app/navbar/navbar.js
+++ builds/development/app/navbar/navbar.js
@@ -1,26 +1,55 @@
 ;(function(){
 	'use strict';
 	angular
-		.module('ngFit.navbar',[
-				   'ngRoute'
-		])
+		.module('ngFit.navbar',['ngRoute'])
 		.controller('AuthController', AuthController);
-
-	function AuthController($scope){
+	AuthController.$inject = ['$scope','$rootScope'];
+	function AuthController($scope,$rootScope){
 		var vm = this;
-		vm.name = null;
-		vm.login = function(){
-			var ref = new Firebase("https://yanfit.firebaseio.com");
-			ref.authWithOAuthPopup("github", function(error, authData) {
+		$rootScope.curPath = 'navbar';
+		vm.name = "";
+		vm.ref = new Firebase("https://yanfit.firebaseio.com");
+		vm.handle = function(promise,event){
+        $.when(promise)
+            .then(
+            	function (authData,event) {
+            		if (event){
+						console.log('stop');
+						event.stopPropagation();
+						event.preventDefault();	
+					}
+        		}, 
+        		function (err) {
+		            console.log(err);
+		        }
+		    );
+		};
+		vm.click = function(event){
+			var socialLoginPromise;    
+            socialLoginPromise = vm.login(event);
+            vm.handle(socialLoginPromise,event);	
+		};
+		vm.login = function(event){
+			var deferred = $.Deferred();
+			vm.ref.authWithOAuthPopup("github", function(error, authData) {
 			  if (error) {
 			    console.log("Login Failed!", error);
-			  } else {
-			  	vm.name = authData.github.username;
-			  	console.log(authData.github.username);
-			    console.log("Authenticated successfully with payload:", authData);
 			  }
-			})
-		}
+			  if (authData) {
+			  	$scope.$apply(function(){
+			  		vm.name = authData.github.username;
+			  		console.log(authData.github.username);
+			    	console.log("Authenticated successfully with payload:", authData);
+			    	deferred.resolve(authData);
+			  	});
+			  	if (event){
+					event.stopPropagation();
+					event.preventDefault();	
+				}
+			  }
+			});
+			return deferred.promise();
+		};
 	}
 })();
 
